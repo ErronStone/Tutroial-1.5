f7b6e047649930656c11adbce7587b1d6c7e764d
ErronStone <wyatta3998@gmail.com>
2019-10-06 16:49:56 -0400

    Animation Tutorial

---
+++ Tutorial 1.5/Assets/scripts/CatScript.cs
@@ -0,0 +1,51 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            Anim.SetInteger("state", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+       {
+            musicSource.Stop();
+            Anim.SetInteger("state", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            Anim.SetInteger("state", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            Anim.SetInteger("state", 0);
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
